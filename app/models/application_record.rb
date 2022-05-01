a4eb91f9a203c5fcc341265842fffd156a6bb325
leonardobotrel <leonardobotrel@gmail.com>
2022-05-01 17:53:08 -0300

    Refactoring the code

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def created_at
-    self[:created_at].strftime("%d/%m/%Y")
+    self[:created_at].strftime("%d/%m/%Y %H:%M")
   end  
 end
